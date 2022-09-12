869355d133f3fbed2ab5362d8c17d4b97ea267f2
Rjndrkha <75615789+Rjndrkha@users.noreply.github.com>
2022-09-12 09:01:14 +0700

    done all

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:sqlite_crud/ui/list_kontak.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Aplikasi Kontak Sqlite',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: const ListKontakPage(),
+    );
+  }
+}
